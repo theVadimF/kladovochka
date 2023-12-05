75a48421ef5b6276258b050efaf20bc343f6424e
Vadim Faust <vadim40328@gmail.com>
2023-12-05 17:19:44 +0700

    WIP user submit form (adaptive done)

--- js/script.js
+++ js/script.js
@@ -10,3 +10,62 @@ $('.mobile_menu_wrap .mask').click(function() {
     $('.mobile_menu_wrap').hide();
   });
 })
+
+const date_slider = new Swiper('.form_wrap .date_slider', {
+  slidesPerView: 2,
+  slidesPerGroup: 1,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.form_wrap .date_wrap .nav.next',
+    prevEl: '.form_wrap .date_wrap .nav.prev',
+  },
+  breakpoints: {
+    890: {
+      slidesPerView: 7,
+      slidesPerGroup: 6,
+    },
+    800: {
+      slidesPerView: 6,
+      slidesPerGroup: 5,
+    },
+    700: {
+      slidesPerView: 7,
+      slidesPerGroup: 6,
+    },
+    650: {
+      slidesPerView: 6,
+      slidesPerGroup: 5,
+    },
+    550: {
+      slidesPerView: 5,
+      slidesPerGroup: 4,
+    }, 
+    465: {
+      slidesPerView: 4,
+      slidesPerGroup: 3,
+    },
+    370: {
+      slidesPerView: 3,
+      slidesPerGroup: 2,
+    }
+  }
+})
+
+
+date_slider.on('slideChange', function () {
+  if (this.activeIndex > 0) {
+    $('.form_wrap .date_wrap .nav.prev').fadeIn('fast');
+  } else {
+    $('.form_wrap .date_wrap .nav.prev').fadeOut('fast');
+  }
+})
+
+$('.form_wrap .date_wrap .date_btn').click(function() {
+  $('.form_wrap .date_wrap .date_btn').removeClass('__active');
+  $(this).addClass('__active');
+})
+
+$('.form_dialog .time_block .time_slots .time_slot_btn').click(function() {
+  $('.form_dialog .time_block .time_slots .time_slot_btn').removeClass('__active');
+  $(this).addClass('__active');
+})
