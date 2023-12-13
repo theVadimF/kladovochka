97b749c87097910c86219cdf868c065bc536139f
Vadim Faust <vadim40328@gmail.com>
2023-12-13 18:32:04 +0700

    WIP admin UI + other fixes

--- js/script.js
+++ js/script.js
@@ -5,8 +5,26 @@ $('header .open_mobile_menu').click(function() {
 
 $('.mobile_menu_wrap .mask').click(function() {
   $('.mobile_menu_wrap').removeClass('__animate_in');
-  $(".mobile_menu_wrap .mask").on("transitionend", function(e){
-    $(this).off(e);
-    $('.mobile_menu_wrap').hide();
-  });
+})
+
+$('.dropdown_wrap .open_dropdown').click(function(e) {
+  let $container = $(this).siblings('.contents_outer');
+  if ($container.hasClass('__shown')) {
+    $container.removeClass('__shown');
+  } else {
+    $('.dropdown_wrap .contents_outer').removeClass('__shown');
+    $(this).siblings('.contents_outer').toggleClass('__shown');
+  }
+})
+
+$('.dropdown_wrap .contents .option').click(function() {
+  let $wrap = $(this).parents('.dropdown_wrap');
+  $wrap.find('.option').removeClass('__selected');
+  if ($wrap.hasClass('__update_text')) {
+    $wrap.find('.current_option').text(
+      $(this).data('value')
+    );
+  }
+  $wrap.children('.contents_outer').removeClass('__shown');
+  $(this).addClass('__selected');
 })
