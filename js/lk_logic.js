4c6b82e7228b6e892338097ac663455aaebc480c
Vadim Faust <vadim40328@gmail.com>
2023-12-19 17:39:30 +0700

    WIP flow logic

--- js/lk_logic.js
+++ js/lk_logic.js
@@ -34,7 +34,6 @@ $('.admin_orders .order_box .status').click(function() {
   $(this).parents('.order_box').toggleClass('__open');
 })
 
-// TODO: move to code scanner
 function check_scan_lock(obj) {
   let $wrapper = $(obj).parents('.boxes_initial');
   return parseInt($wrapper.data('lock')) < parseInt($(obj).data('step'));
@@ -88,7 +87,8 @@ $('.popup .float_buttons .replace.__img').click(function() {
   $('#' + $(this).data('target-id')).children(`.box_img_input[data-step=${step}]`).click();
 })
 
-
+// TODO(vf) make this check the current box only
+// TODO(vf) move fix to code_scanner
 $('.admin_orders .boxes_initial .box_btn').on('boxBtnUpdate', function() {
   let flag = true;
   $('.admin_orders .boxes_initial .box_btn').each(function() {
@@ -166,4 +166,100 @@ $('.popup.__code .close').click(function() {
   $('.popup.__code').removeClass('__shown');
 })
 
+$('.admin_orders .order_box .bottom .accept_initial').click(function() {
+  $(this).addClass('__hidden');
+  let $hidden_block = $(this).parents('.hidden_block');
+  let id = $(this).parents('.order_box').data('order-num');
+  console.log(id);
+  $hidden_block.append(`
+    <div class="boxes_initial" data-lock="0" data-order-num="${id}" data-id-origin="0">
+    </div>
+    <div class="bottom_btns box_wrap">
+      <button class="beige_btn bottom_btn __white add_box">Добавить коробку / место</button>
+      <button class="beige_btn bottom_btn accept_btn __hidden" disabled autocomplete="off">Принять</button>
+    </div>
+  `);
 
+  init_box_add();
+})
+
+function init_box_add() {
+  $('.admin_orders .order_box .bottom_btns .add_box').click(function() {
+    $(this).text("Добавить еще коробку");
+    $(this).siblings('.accept_btn').removeClass('__hidden');
+    let $wrapper = $(this).parents('.hidden_block').children('.boxes_initial')
+    // console.log($wrapper[0]);
+    add_box($wrapper);
+  })
+}
+
+// [0] - initial text, [1] - success text, [2] - show box number bool
+function get_step1_text(type) {
+  switch (key) {
+    case "courier_receive":
+      return [
+        `Добавьте штрих-код коробки <span class="box_number">0</span>`,
+        `Штрих-код добавлен`,
+        false
+      ]
+      break;
+  
+    default:
+      console.log("Unknown type")
+      break;
+  }
+}
+
+// TODO(vf) add toggle for data-show-number in code_scanner
+function add_box($wrapper) {
+  let id = $wrapper.data('order-num');
+  let box_id = parseInt($wrapper.data('id-origin')) + 1;
+  // [0] - initial text, [1] - success text, [2] - show box number bool
+  let button_text = get_step1_text($wrapper.data('type'));
+  $wrapper.data('id-origin', box_id);
+  $wrapper.append(`
+    <div class="box_wrap" id="box${id}_${box_id}">
+      <div class="box_top">
+        <p class="text bold_info box_topper __shown">Коробка <span class="box_number">0</span></p>
+        <button class="transparent_btn box_btn delete_box">Удалить</button>
+        <p class="text box_status_final"><b class="bold_info">Статус: </b><span class="status_text">Принят у клиента</status></p>
+      </div>
+      <div class="box_inner_wrap">
+        <button class="transparent_btn box_btn box_scan_btn text" data-step="1" data-success-text="Штрих-код добавлен" data-show-number="0" data-scan-type="qrcode">
+          <span>
+            <span class="box_status __qr">Добавьте штрих-код коробки <span class="box_number">0</span></span>
+            <span class="box_number_wrap"> <span class="box_number">0</span> добавлен</span>
+          </span>
+          <img src="./img/ico/img_placeholder.png" alt="" class="preview __qr">
+        </button>
+        <input type="file" class="box_img_input" accept="image/*" capture data-step="1">
+        <button class="transparent_btn box_btn box_img_btn text" disabled autocomplete="off" data-step="1" data-success-text="Фото добавлено" data-show-number="0">
+          <span>
+            <span class="box_status __img">Добавьте фото коробки <span class="box_number">0</span></span>
+            <span class="box_number_wrap"> <span class="box_number">0</span> добавлено</span>
+          </span>
+          <span class="box_status __img">Добавьте фото коробки <span class="box_number">0</span></span>
+          <img src="./img/ico/img_placeholder.png" alt="" class="preview __img">
+        </button>
+        <p class="text box_status_final __hidden"><b class="bold_info">Статус: </b>Принят у клиента</p>
+      </div>
+    </div>
+  `)
+  set_box_numbers($wrapper);
+  init_delete_box();
+}
+
+function set_box_numbers($wrapper) {
+  $wrapper.children('.box_wrap').each(function(index) {
+    $(this).find('.box_number').text(index + 1);
+  })
+}
+
+function init_delete_box() {
+  $('.admin_orders .boxes_initial .delete_box').click(function() {
+    let $box_wrap = $(this).parents('.box_wrap');
+    let $wrapper = $(this).parents('.boxes_initial');
+    $box_wrap.remove();
+    set_box_numbers($wrapper);
+  })
+}
